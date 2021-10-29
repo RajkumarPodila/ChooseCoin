ccc868d7646f588026f673ddb5fe96e948e0443d
rajvrinda <rajk.dev1010@gmail.com>
2021-10-29 18:18:10 +0530

    der

--- thrd/ViewController.swift
+++ thrd/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print("hyderabad")
+        print("warangal")
         // Do any additional setup after loading the view.
     }
 
